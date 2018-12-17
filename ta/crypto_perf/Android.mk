d556f1bc0fe088557a4b4ab5a17424fc0c7a43d6
Cedric Neveux <cedric.neveux@nxp.com>
2018-12-17 13:30:23 +0100

    TEE-127 CAAM Crypto Performance
    Implementation of the test application and TA
    xhost test:
     - parse test command line and test file
     - call TA Crypt Performance to prepare algorithm
     - call TA to execute algorithm
    
    TA test operations:
     - Cipher done
     - Digest done
     - MAC done
     - Asymmetric Cipher done (RSAES and RSA_NOPAD)
     - Asymemtric Digest done (RSA/DSA)
     - Derive Key (DH/ECDH) done
     - Authenticated Encryption (CCM/GCM) done
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

---
+++ ta/crypto_perf/Android.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH := $(call my-dir)
+
+local_module := 690d2100-dbe5-11e6-bf26-cec0c932ce01.ta
+include $(BUILD_OPTEE_MK)
---
+++ ta/crypto_perf/include/ta_crypto_perf.h
@@ -0,0 +1,71 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018 NXP
+ */
+
+#ifndef __TA_CRYPTO_PERF_H__
+#define __TA_CRYPTO_PERF_H__
+
+#include <ta_crypto_perf_test.h>
+#include <tee_api.h>
+#include <utee_defines.h>
+
+#define FORCE_DEBUG 0
+
+#ifndef FORCE_DEBUG
+#define CHECK(res, name, action) do { \
+		if ((res) != TEE_SUCCESS) { \
+			DMSG(name ": 0x%08x", (res)); \
+			action \
+		} \
+	} while (0)
+#else
+#define CHECK(res, name, action) do { \
+		if ((res) != TEE_SUCCESS) { \
+			MSG(name ": 0x%08x", (res)); \
+			action \
+		} \
+	} while (0)
+#endif
+
+uint32_t get_nb_algo(void);
+uint32_t get_size_name_alg_list(void);
+void     copy_name_alg_list(char *buffer);
+uint32_t get_alg_id(char *name);
+
+
+/* Cipher Functions */
+TEE_Result TA_CipherPrepareAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_CipherProcessAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_CipherFreeAlgo(uint32_t algo, TEE_Param params[4]);
+
+/* Digest Functions */
+TEE_Result TA_DigestPrepareAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_DigestProcessAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_DigestFreeAlgo(uint32_t algo, TEE_Param params[4]);
+
+/* Mac Functions */
+TEE_Result TA_MacPrepareAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_MacProcessAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_MacFreeAlgo(uint32_t algo, TEE_Param params[4]);
+
+/* Asymmetric Cipher Functions */
+TEE_Result TA_AsymCipherPrepareAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_AsymCipherProcessAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_AsymCipherFreeAlgo(uint32_t algo, TEE_Param params[4]);
+
+/* Asymmetric Digest Functions */
+TEE_Result TA_AsymDigestPrepareAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_AsymDigestProcessAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_AsymDigestFreeAlgo(uint32_t algo, TEE_Param params[4]);
+
+/* Key Derivation Functions */
+TEE_Result TA_KeyDerivePrepareAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_KeyDeriveProcessAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_KeyDeriveFreeAlgo(uint32_t algo, TEE_Param params[4]);
+
+/* Authenticated Encryption Functions */
+TEE_Result TA_AuthenEncPrepareAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_AuthenEncProcessAlgo(uint32_t algo, TEE_Param params[4]);
+TEE_Result TA_AuthenEncFreeAlgo(uint32_t algo, TEE_Param params[4]);
+#endif
